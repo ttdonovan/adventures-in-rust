ca2f9a12a68e9c31e6c86a717b680295bf66106c
Tanner Donovan <ttdonovan@gmail.com>
2016-10-18 23:42:07 -0700

    arcade-rs: switching views part 1

--- arcade-rs/src/main.rs
+++ arcade-rs/src/main.rs
@@ -3,36 +3,64 @@ extern crate sdl2;
 mod phi;
 mod views;
 
-use sdl2::pixels::Color;
-use phi::Events;
+use phi::{Events, Phi, View, ViewAction};
 
 fn main() {
     // initialize SDL2
     let sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
 
     // create a window
     let window = video.window("ArcadeRS Shooter", 800, 600)
         .position_centered().opengl()
         .build().unwrap();
 
-    let mut renderer = window.renderer()
-        .accelerated()
-        .build().unwrap();
+    // create the context
+    let mut context = Phi {
+        events: Events::new(sdl_context.event_pump().unwrap()),
+        renderer: window.renderer()
+            .accelerated()
+            .build().unwrap(),
+    };
 
-    // prepare the events record
-    let mut events = Events::new(sdl_context.event_pump().unwrap());
+    // create the default view
+    let mut current_view: Box<View> = Box::new(::views::DefaultView);
+
+    // frame timing
+    let interval = 1_000 / 60;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
 
     loop {
-        events.pump();
+        // frame timing (bis)
+        let now = timer.ticks();
+        let dt = now - before;
+        let elapsed = dt as f64 / 1_000.0;
 
-        if events.now.quit || events.now.key_escape == Some(true) {
-            break;
+        // if the time elapsed since the last frame is too small, wait out the
+        // the difference and try again
+        if dt < interval {
+            timer.delay(interval - dt);
+            continue;
         }
 
-        // render a full black window
-        renderer.set_draw_color(Color::RGB(0, 0, 0));
-        renderer.clear();
-        renderer.present();
+        before = now;
+        fps += 1;
+
+        if now - last_second > 1_000 {
+            println!("FPS: {}", fps);
+            last_second = now;
+            fps = 0;
+        }
+
+        // logic and rendering
+        context.events.pump();
+
+        match current_view.render(&mut context, 0.01) {
+            ViewAction::None => context.renderer.present(),
+            ViewAction::Quit => break,
+        }
     }
 }
---
+++ arcade-rs/src/views/mod.rs
@@ -0,0 +1,22 @@
+use phi::{Phi, View, ViewAction};
+use sdl2::pixels::Color;
+
+pub struct DefaultView;
+
+impl View for DefaultView {
+    fn render(&mut self, context: &mut Phi, _: f64) -> ViewAction {
+        let renderer = &mut context.renderer;
+        let events = &mut context.events;
+
+
+        if events.now.quit || events.now.key_escape == Some(true) {
+            return ViewAction::Quit;
+        }
+
+        // render a full black window
+        renderer.set_draw_color(Color::RGB(0, 0, 0));
+        renderer.clear();
+
+        ViewAction::None
+    }
+}
