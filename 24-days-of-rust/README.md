f82e0cd95d7b07bd4f8646778da8efc107f344a9
Tanner Donovan <ttdonovan@gmail.com>
2016-12-29 10:45:12 -0800

    24 days of rust: reqwest

---
+++ 24-days-of-rust/README.md
@@ -0,0 +1,7 @@
+# 24 Days of Rust
+
+* https://siciarz.net
+
+## Topics
+
+* [Reqwest](https://siciarz.net/24-days-rust-reqwest/)
---
+++ 24-days-of-rust/reqwest/src/main.rs
@@ -0,0 +1,19 @@
+extern crate reqwest;
+
+use std::io::Read;
+
+fn main() {
+    let mut response = reqwest::get("https://httpbin.org/status/418")
+        .expect("Failed to send request");
+
+    println!("{}", response.status());
+
+    for header in response.headers().iter() {
+      println!("{}: {}", header.name(), header.value_string());
+    }
+
+    let mut buf = String::new();
+    response.read_to_string(&mut buf).expect("Failed to read response");
+
+    println!("{}", buf);
+}
