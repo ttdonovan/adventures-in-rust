965164481d368aca086bf6d70d4c3cd81d6aa649
Tanner Donovan <ttdonovan@gmail.com>
2017-07-11 20:37:38 -0700

    roguelike: part 1 - render

--- roguelike/REAMD.md
+++ roguelike/REAMD.md
@@ -3,3 +3,9 @@
 This Roguelike was built by following along with the tutorial.
 
 http://tomassedovic.github.io/roguelike-tutorial/
+
+## Building on Mac OS X
+
+```
+$ brew install pkg-config sdl
+```
--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -1,3 +1,29 @@
+extern crate tcod;
+
+use tcod::console::*;
+use tcod::colors;
+
+// size of the window
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+// 20 frames-per-second maximum
+const LIMIT_FPS: i32 = 20;
+
 fn main() {
-    println!("Hello, world!");
+    let mut root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    while !root.window_closed() {
+        root.set_default_foreground(colors::WHITE);
+        root.put_char(1, 1, '@', BackgroundFlag::None);
+        root.flush();
+        root.wait_for_keypress(true);
+    }
 }
