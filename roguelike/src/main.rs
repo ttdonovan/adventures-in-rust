afb6864a9c0ddbd156b52501b511d50009d45446
Tanner Donovan <ttdonovan@gmail.com>
2017-07-11 20:37:48 -0700

    roguelike: part 1 - movement

--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -10,6 +10,32 @@ const SCREEN_HEIGHT: i32 = 50;
 // 20 frames-per-second maximum
 const LIMIT_FPS: i32 = 20;
 
+fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+    match key {
+        // Alt+Enter: toogle full screen
+        Key { code: Enter, alt: true, .. } => {
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        },
+
+        // exit game
+        Key { code: Escape, .. } => return true,
+
+        // movements
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        _ => {},
+    }
+    false
+}
+
 fn main() {
     let mut root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
@@ -20,10 +46,21 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !root.window_closed() {
         root.set_default_foreground(colors::WHITE);
-        root.put_char(1, 1, '@', BackgroundFlag::None);
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+
         root.flush();
-        root.wait_for_keypress(true);
+
+        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+
+        // handle keys and exit game if needed
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        if exit {
+            break
+        }
     }
 }
